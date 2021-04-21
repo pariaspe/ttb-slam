ba07495ef4a8cbe3e7dcd625fd58f850a6e7d4f3
RocaPiedra <pablo_rocasg97@hotmail.com>
2021-04-21 23:40:56 +0200

    includes function to empty infinite positions

--- laserScan_sub.py
+++ laserScan_sub.py
@@ -1,23 +1,29 @@
 from __future__ import print_function
 import rospy
+import numpy as np
 #import turtlebot3_msgs
 #from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
 def callback(msg):
-    print('in callback preprint')
     print('number of range points:', len(msg.ranges))
-    print('in callback')
+    print('distances:\n 0 degrees:%.2f'% msg.ranges[0], 'm \n 90 degrees: %.2f'% msg.ranges[89], 'm\n 180 degrees: %.2f'% msg.ranges[179], 'm\n 270 degrees: %.2f'% msg.ranges[269],'m')
+    rospy.sleep(2)
+
+#filtra los valores de inf quedando None en sus posiciones
+def mapper(ranger):
+    instant_map = [None] * len(ranger.ranges)
+    for i in range(len(ranger.ranges)):
+        if ranger.ranges[i] != np.inf:
+            instant_map[i] = ranger.ranges[i]
+    rospy.sleep(5)
+    print(list(instant_map))
 
 rospy.init_node('scan_sub_node')
-sub = rospy.Subscriber('/scan', LaserScan, callback, queue_size=1)
-print('subscriber declared')
-message = rospy.wait_for_message("/scan", LaserScan)
 
-print('range at 0 degrees:', message.ranges[0])
-print('range at 90 degrees:', message.ranges[89])
-print('range at 180 degrees:', message.ranges[179])
-print('range at 270 degrees:', message.ranges[269])
+sub = rospy.Subscriber('/scan', LaserScan, callback)
 
-rospy.spin
+mapping = rospy.Subscriber('/scan', LaserScan, mapper) 
+
+rospy.spin()
 
