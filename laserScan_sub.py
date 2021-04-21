0ea39f1cbebf9e5f6d81a931d826d356e123c321
RocaPiedra <pablo_rocasg97@hotmail.com>
2021-04-21 21:39:33 +0200

    wait for message makes the subscriber work

--- laserScan_sub.py
+++ laserScan_sub.py
@@ -1,5 +1,4 @@
-#/usr/bin/env python
-
+from __future__ import print_function
 import rospy
 #import turtlebot3_msgs
 #from geometry_msgs.msg import Twist
@@ -13,5 +12,12 @@ def callback(msg):
 rospy.init_node('scan_sub_node')
 sub = rospy.Subscriber('/scan', LaserScan, callback, queue_size=1)
 print('subscriber declared')
+message = rospy.wait_for_message("/scan", LaserScan)
+
+print('range at 0 degrees:', message.ranges[0])
+print('range at 90 degrees:', message.ranges[89])
+print('range at 180 degrees:', message.ranges[179])
+print('range at 270 degrees:', message.ranges[269])
+
 rospy.spin
 
