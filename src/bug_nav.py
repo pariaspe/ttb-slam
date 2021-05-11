a8ff7f1ca5ca484a613af5bc5120832603155e43
Aitor Ezkerra <aitor_ezkerra@hotmail.com>
2021-05-11 14:18:54 +0200

    mistakes solved

--- src/bug_nav.py
+++ src/bug_nav.py
@@ -22,13 +22,14 @@ def detect_wall(ranges):
     return idx_min
 
 def remap_angle(angle):
+    # Remaps the angle from [180, -180] to [0, 360]
     if angle <= 0:
         return -angle
     elif angle > 0:
         return 360 - angle
 
 def bug_initialization(turtle):
-    res = 1  # Resolution of the advancing angle
+    res = 2  # Resolution of the advancing angle
     angle = 90      # Angle in which it is wanted to advance
     wall_idx = 0
     while wall_idx not in range(angle-res, angle + res + 1):
@@ -47,25 +48,31 @@ def bug_initialization(turtle):
 
 def calculate_wall_angle(full_distances, wall_angle):
     # Calculate the angle to the wall
-    wall_side = math.sqrt(full_distances[-wall_angle]**2 + full_distances[0]**2 - 2*full_distances[-wall_angle]*full_distances[0]*math.cos(math.radians(wall_angle)))
-    angle = round((360 - math.degrees(math.asin((full_distances[-wall_angle]*math.sin(math.radians(wall_angle)))/wall_side))*2)/2)
-    print(angle)
+    wall_side = math.sqrt(full_distances[wall_angle-15]**2 + full_distances[wall_angle]**2 - 2*full_distances[wall_angle-15]*full_distances[wall_angle]*math.cos(math.radians(15)))
+    angle = round((360 - math.degrees(math.asin((full_distances[wall_angle-15]*math.sin(math.radians(15)))/wall_side))*2)/2)
+    
     return angle
 
 def rotate_against_wall(turtle, angle, direction):
     turtle.stop()
-    error = 4       # Little correction
-    angle = angle - error
+    #error = 4       # Little correction
+    angle = angle #- error
     print(f'Rotating {angle} degrees')
     pose = turtle.get_estimated_pose()
-    initial_orientation = math.degrees(quat_to_euler(pose.orientation)[2])
-    remap_angle(initial_orientation)
+
+    initial_orientation = math.degrees(quat_to_euler(pose.orientation)[2])  # Save initial orientation
+    initial_orientation = remap_angle(initial_orientation)
     turtle_orientation = initial_orientation
-    while abs(turtle_orientation - initial_orientation) < angle:  # need to implement angle
+
+    while abs(turtle_orientation - initial_orientation) < angle:  # Rotate until reached the angle
         turtle.set_vel(az=0.3 * direction)
         time.sleep(0.05)
         turtle_orientation = math.degrees(quat_to_euler(turtle.get_estimated_pose().orientation)[2])
-        remap_angle(turtle_orientation)
+        turtle_orientation = remap_angle(turtle_orientation)
+
+        if initial_orientation + angle > 360:   # Check if the turn will overflow
+            if turtle_orientation < 100:
+                turtle_orientation += 360
         
 
 
@@ -81,7 +88,7 @@ def main():
 
     timer = 0
     position_error = 0.5
-    wall_angle = 15
+    #wall_angle = 15
     bug_initialized = False
     while turtle.is_running():
         pose = turtle.get_estimated_pose()
@@ -94,49 +101,39 @@ def main():
         
         turtle.set_vel(vx=0.3)
         time.sleep(0.05)
-        wall_dist = full_distances[90]
+        
 
         # If the robot is not fully parallel to the wall
-        if full_distances[85] - full_distances[95] > 0.03:
+        if full_distances[85] - full_distances[95] > 0.02:
             print("Adjusting to the left...")
-            turtle.stop()
-            turtle.set_vel(az=0.2)
-            time.sleep(0.5)
-            turtle.set_vel(az=0)
-        elif full_distances[95] - full_distances[85] > 0.03:
+            turtle.set_vel(az=0.2, vx=0.3)
+            time.sleep(0.2)
+            
+        elif full_distances[95] - full_distances[85] > 0.02:
             print("Adjusting to the right...")
-            turtle.stop()
-            turtle.set_vel(az=-0.2)
-            time.sleep(0.5)
-            turtle.set_vel(az=0)
+            turtle.set_vel(az=-0.2, vx=0.3)
+            time.sleep(0.2)
 
         if full_distances[0] < 0.8: # If there is wall in front
             print('Detected wall in front')
             turtle.stop()   # Stop turtle
-            angle = calculate_wall_angle(full_distances, wall_angle)
+            angle = calculate_wall_angle(full_distances, 0)
             rotate_against_wall(turtle, angle, -1)      
         
-        if full_distances[75] - full_distances[90] > 0.5: # gap on left side
-            print("Detected a gap on the side")
+        if full_distances[75] - full_distances[90] > 1: # gap on left side
+            print("Detected a gap on the side")         
+
             turtle.set_vel(vx=0.3)
             time.sleep(3)
             turtle.stop()
-            if full_distances[90] > 2:      # If the turn is more than 90 degrees
-                turtle.set_vel(vx=0.3)      # Advance to avoid the wall after rotation
-                time.sleep(3)
-                rotate_against_wall(turtle, 90, 1)
-                turtle.set_vel(vx=0.3)      # Advance to see where the next wall is
-                time.sleep(3)
-                angle = calculate_wall_angle(full_distances, wall_angle + 90)
-                rotate_against_wall(turtle, angle, 1)
-            else:                           # If the turn is less than 90 degrees
-                angle = calculate_wall_angle(full_distances, wall_angle + 90)
-                rotate_against_wall(turtle, angle, 1)
+            rotate_against_wall(turtle, 90, 1)      # assumed it is a 90 degree turn
+            turtle.set_vel(vx=0.3)      # Advance to see where the next wall is
+            time.sleep(3)
+            
         
-        if timer > 100:
+        if timer > 100:     # Little delay to give time to move from original position
             # Check whether the ttb arrived to the initial position
             current_position = turtle.get_estimated_pose().position
-            
             if abs(current_position.y - initial_position.y) < position_error and abs(current_position.x - initial_position.x) < position_error:
                 turtle.stop()
                 break
